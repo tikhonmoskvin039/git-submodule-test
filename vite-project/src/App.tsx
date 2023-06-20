fd3f1041560c7efaa0c934a2c641ee13a0ea5f09
Tikhon Moskvin <tihonoskvin@mail.ru>
2023-06-20 23:16:46 +0300

    upd main 2

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,20 @@
-import MyButton from "./git-submodule-globals/components/MyButton";
+// import './App.css'
+
+import MyButton from "./git-submodule-globals/components/MyButton"
 
 function App() {
+
+
   return (
     <>
       <div>
-        <MyButton />
-        <MyButton />
-        dfsdf
+      hello
       </div>
+      <MyButton/>
+      <MyButton/>
+      <MyButton/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
